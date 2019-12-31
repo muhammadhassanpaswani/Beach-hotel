aff1214c3dc503a029d3909a984424fdd658b386
muhammadhassanpaswani <hassanwebpaswani@gmail.com>
2020-01-01 00:45:24 +0500

    loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,6 +3,7 @@ import loader from "../images/gif/loading-arrow.gif";
 export default function Loading() {
   return (
     <div className="loading">
+    
       <h4>Rooms data Loading...</h4>
       <img src={loader} alt="" />
     </div>
